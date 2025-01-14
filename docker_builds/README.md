c94300b85095581f27584739387c877e11ec10ae
JuliusRoeder <julius@roeder.dev>
2025-01-14 13:43:41 +0100

    Builds three docker images and includes RCCL and OSU tests

--- docker_builds/README.md
+++ docker_builds/README.md
@@ -2,34 +2,32 @@ Exploring the different options for building a docker image without proprietary 
 This is based on the LUMI docker build files found here:
 
 https://github.com/sfantao/lumi-containers/tree/lumi-sep2024
-Especially the 
 
 
 The main aim is to be able to build docker base images without needing a system similar to LUMI.  
 
 ### TODO
-- tests for MPI (OSU)
 - GROMACS or similar? https://www.gromacs.org/tutorial_webinar.html
 - Speed tests for pytorch
-
-### Questions:
-- Can we just have MPI in the container and bind mount libfabric? 
-  - Or what is the "complete" MPI replacement??
-
+- OSU GPU Aware MPI somehow
 
 ## Libfabric replacement only
 
 Goal: 
 - Build container with libfabric, MPI etc. 
 - And **ONLY** replace libfabric.
+- RCCL tests are included and compiled with MPICH
+- OSU tests --> CPU Only
 
-The Libfabric base works, at least to the point where the single GPU and multi GPU PyTorch examples work, as well as the RCCL test without MPICH. 
-- RCCL tests are included but cannot run as they need to be compiled with MPICH. 
-- TODO: OSU tests??
-- TODO: Include MPICH or OpenMPI. Only replace libfabric
+The Libfabric base works, at least to the point where the single GPU and multi GPU PyTorch examples work, as well as the RCCL test without MPICH.
+TODO: Test new image that also includes MPICH
 
 ## Libfabric & Mpich replacement (i.e. complete replacement?)
 
 Goal: 
 - Build container with libfabric, MPI etc. 
 - Replace libfabric and MPICH.
+- RCCL tests are included and compiled with MPICH
+- OSU tests --> CPU Only
+
+TODO: Test 
--- docker_builds/build_docker.py
+++ docker_builds/build_docker.py
@@ -19,43 +19,78 @@ def show_progress(line, console):
 
     console.log(log_message, markup=True)
 
-
+#Todo: Define versions for DOCKER
 
 docker_files_path_prefix = "common_docker_defs"
 
 # Define which docker files to use
-docker_files = ["Dockerfile.header",
-                "Dockerfile.define_versions",
-                "Dockerfile.install_basic_dependencies",
-                "Dockerfile.install_rocm",
-                "Dockerfile.fake_rocm_gpu_info",
-                "Dockerfile.install_libfabric",
-                "Dockerfile.install_rccl",
-                "Dockerfile.install_mpich",
-                "Dockerfile.install_rccl_tests"]
+docker_header = "Dockerfile.header"
+docker_define_versions = "Dockerfile.define_versions"
+docker_install_basic_dependencies = "Dockerfile.install_basic_dependencies"
+docker_install_rocm = "Dockerfile.install_rocm"
+docker_fake_rocm_gpu_info = "Dockerfile.fake_rocm_gpu_info"
+docker_install_libfabric = "Dockerfile.install_libfabric"
+docker_install_rccl = "Dockerfile.install_rccl"
+docker_install_mpich = "Dockerfile.install_mpich"
+docker_install_rccl_tests = "Dockerfile.install_rccl_tests"
+docker_install_osu = "Dockerfile.install_osu"
+docker_install_osu_gpu = "Dockerfile.install_osu_gpu" # TODO!
+docker_remove_libfabric = "Dockerfile.remove_libfabric"
+docker_remove_mpich = "Dockerfile.remove_mpich"
 
-# Generate the docker fileobject
-dockerfile_path = 'additional_docker_files/Dockerfile'
-with open('additional_docker_files/Dockerfile', 'w') as outfile:
-    for docker_file in docker_files:
-        with open(os.path.join(docker_files_path_prefix, docker_file)) as infile:
-            for line in infile:
-                outfile.write(line)
+# For building multiple images
+images_to_build = {"Docker_only" : [],
+                   "libfabric_hybrid": [],
+                    "mpich_hybrid": [],}
+
+# Base image with all libraries and tests. Not removing libfabric or mpich
+images_to_build["Docker_only"] += [docker_header,
+                                        docker_define_versions,
+                                        docker_install_basic_dependencies,
+                                        docker_install_rocm,
+                                        docker_fake_rocm_gpu_info,
+                                        docker_install_libfabric,
+                                        docker_install_rccl,
+                                        docker_install_mpich,
+                                        docker_install_rccl_tests,
+                                        docker_install_osu
+                                        ]
+
+# Libfabric_hybrid --> Removing libfabric
+images_to_build["libfabric_hybrid"] = images_to_build["Docker_only"].copy()
+images_to_build["libfabric_hybrid"].append(docker_remove_libfabric)
+
+# mpich_hybrid --> Removing libfabric and MPICH
+images_to_build["mpich_hybrid"] = images_to_build["Docker_only"].copy()
+images_to_build["mpich_hybrid"].append(docker_remove_libfabric)
+images_to_build["mpich_hybrid"].append(docker_remove_mpich)
+
+print(images_to_build)
+
+# Build all images one go
+for current_image_name, current_image_files in images_to_build.items():
+    # Generate the docker fileobject
+    dockerfile_path = 'additional_docker_files/Dockerfile'
+    with open('additional_docker_files/Dockerfile', 'w') as outfile:
+        for docker_file in current_image_files:
+            with open(os.path.join(docker_files_path_prefix, docker_file)) as infile:
+                for line in infile:
+                    outfile.write(line)
 
 
-#Path to dockerfile location
-old_dir = os.getcwd()
-path = os.path.join(old_dir, 'additional_docker_files')
+    #Path to dockerfile location
+    old_dir = os.getcwd()
+    path = os.path.join(old_dir, 'additional_docker_files')
 
-console = Console()
-with console.status("[bold green]Working on tasks...") as status:
-    client = docker.from_env()
-    try:
-        resp = client.api.build(rm=True, quiet=False, tag="lumi_images:test", path=path) #fileobj=f,
-        for line in resp:
-            line_dict = json.loads(line.decode('utf-8'))
-            show_progress(line_dict, console)
-    except DockerException as e:
-        print(f"An error occurred: {e}")
+    console = Console()
+    with console.status("[bold green]Working on tasks...") as status:
+        client = docker.from_env()
+        try:
+            resp = client.api.build(rm=True, quiet=False, tag="lumi_images:" + current_image_name, path=path) #fileobj=f, , nocache=True
+            for line in resp:
+                line_dict = json.loads(line.decode('utf-8'))
+                show_progress(line_dict, console)
+        except DockerException as e:
+            print(f"An error occurred: {e}")
 
-os.remove(dockerfile_path)
+    os.remove(dockerfile_path)
