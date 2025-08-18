c04a0b5014b3b791dbca29be4a1084476020d079
Joachim Sødequist <139451906+joasode@users.noreply.github.com>
2025-08-18 15:40:41 +0200

    Change append to overwrite in osu wi4mpi

--- MPI 5.0 ABI with Wi4MPI/osu-benchmarks/osu-wi4mpi.sh
+++ MPI 5.0 ABI with Wi4MPI/osu-benchmarks/osu-wi4mpi.sh
@@ -4,11 +4,11 @@ spack unload -a
 spack load wi4mpi
 
 echo "Running Wi4MPI MPI ABI interface with MPICH runtime binaries"
-mpirun -T mpich -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/startup/osu_init >> results/mpich-interface-init.txt
-mpirun -T mpich -np 2 osu-micro-benchmarks-5.9-wi4mpi/mpi/pt2pt/osu_bibw >> results/mpich-interface-bw.txt
-mpirun -T mpich -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/collective/osu_allreduce >> results/mpich-interface-allred.txt
+mpirun -T mpich -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/startup/osu_init > results/mpich-interface-init.txt
+mpirun -T mpich -np 2 osu-micro-benchmarks-5.9-wi4mpi/mpi/pt2pt/osu_bibw > results/mpich-interface-bw.txt
+mpirun -T mpich -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/collective/osu_allreduce > results/mpich-interface-allred.txt
 
 echo "Running Wi4MPI MPI ABI interface with OpenMPI runtime binaries"
-mpirun -T openmpi -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/startup/osu_init >> results/ompi-interface-init.txt
-mpirun -T openmpi -np 2 osu-micro-benchmarks-5.9-wi4mpi/mpi/pt2pt/osu_bibw >> results/ompi-interface-bw.txt
-mpirun -T openmpi -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/collective/osu_allreduce >> results/ompi-interface-allred.txt
+mpirun -T openmpi -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/startup/osu_init > results/ompi-interface-init.txt
+mpirun -T openmpi -np 2 osu-micro-benchmarks-5.9-wi4mpi/mpi/pt2pt/osu_bibw > results/ompi-interface-bw.txt
+mpirun -T openmpi -np 4 osu-micro-benchmarks-5.9-wi4mpi/mpi/collective/osu_allreduce > results/ompi-interface-allred.txt
