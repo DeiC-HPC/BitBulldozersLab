47a6c1b5b16c710343a5c3f434c04c4450704cf0
Joachim Sødequist <139451906+joasode@users.noreply.github.com>
2025-08-18 15:40:12 +0200

    Change append to overwrite in hello-world

--- MPI 5.0 ABI with Wi4MPI/hello-world/run.sh
+++ MPI 5.0 ABI with Wi4MPI/hello-world/run.sh
@@ -3,19 +3,19 @@ spack load mpich
 spack load wi4mpi
 echo 'Running MPI ABI'
 echo 'Running MPICH compiled binary run with OpenMPI'
-mpirun -F mpich -T openmpi -n 4 ./hello-preload-mpich >> results/preload-mpich.txt
+mpirun -F mpich -T openmpi -n 4 ./hello-preload-mpich > results/preload-mpich.txt
 
 spack unload -a
 spack load openmpi
 spack load wi4mpi
 echo 'Running OpenMPI compiled binary run with MPICH'
-mpirun -F openmpi -T mpich -np 4 ./hello-preload-openmpi >> results/preload-openmpi.txt
+mpirun -F openmpi -T mpich -np 4 ./hello-preload-openmpi > results/preload-openmpi.txt
 
 spack unload -a
 spack load wi4mpi
 echo 'Running Wi4MPI interface compiled binary run with OpenMPI'
-mpirun -T openmpi -np 4 ./hello-interface-wi4mpi >> results/interface-openmpi.txt
+mpirun -T openmpi -np 4 ./hello-interface-wi4mpi > results/interface-openmpi.txt
 echo 'Running Wi4MPI interface compiled binary run with MPICH'
-mpirun -T mpich -np 4 ./hello-interface-wi4mpi >> results/interface-mpich.txt
+mpirun -T mpich -np 4 ./hello-interface-wi4mpi > results/interface-mpich.txt
 
 echo 'Done'
